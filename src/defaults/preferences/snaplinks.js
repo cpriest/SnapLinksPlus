deb52727c490f81e599e066ea863a992fd1ebbcb
rautava <devnull@localhost>
2008-12-04 17:51:07 +0000

    Imported the version 0.0.4.

---
+++ src/defaults/preferences/snaplinks.js
@@ -0,0 +1,14 @@
+
+pref("extensions.snaplinks.button", 2);
+pref("extensions.snaplinks.defaultaction", 0);
+pref("extensions.snaplinks.openmouseleave", false);
+pref("extensions.snaplinks.shownumber", true);
+pref("extensions.snaplinks.multiline", false);
+pref("extensions.snaplinks.multifonts", false);
+
+pref("extensions.snaplinks.linkspicker","#FF0000");
+pref("extensions.snaplinks.linksthick",1);
+pref("extensions.snaplinks.drawpicker","#30AF00");
+pref("extensions.snaplinks.drawthick",3);
+
+pref("extensions.snaplinks@snaplinks.net.description", "chrome://snaplinks/locale/snaplinks.properties");  
