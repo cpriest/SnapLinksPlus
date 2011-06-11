ec6eef2151ff60f2599b144cf4d5a2ebbb51fc50
cpriest <devnull@localhost>
2011-06-11 21:19:43 +0000

    CHANGE: Changed the options dialog and some locale strings

--- src/defaults/preferences/snaplinks.js
+++ src/defaults/preferences/snaplinks.js
@@ -18,16 +18,14 @@
  *  along with Snap Links.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-pref("extensions.snaplinks@snaplinks.mozdev.org.description", "chrome://snaplinks/locale/snaplinks.properties");  
+pref('extensions.snaplinks@snaplinks.mozdev.org.description', 'chrome://snaplinks/locale/snaplinks.properties');  
 
-pref("extensions.snaplinks.button", 2);
-pref("extensions.snaplinks.defaultaction", 0);
-pref("extensions.snaplinks.openmouseleave", false);
-pref("extensions.snaplinks.shownumber", true);
-pref("extensions.snaplinks.multiline", false);
-pref("extensions.snaplinks.multifonts", false);
+pref('extensions.snaplinks.button', 2);
+pref('extensions.snaplinks.defaultaction', 'OpenTabs');
+pref('extensions.snaplinks.HideSelectionOnMouseLeave', false);
+pref('extensions.snaplinks.shownumber', true);
 
-pref("extensions.snaplinks.linkspicker","#FF0000");
-pref("extensions.snaplinks.linksthick",1);
-pref("extensions.snaplinks.drawpicker","#30AF00");
-pref("extensions.snaplinks.drawthick",3);
+pref('extensions.snaplinks.linkspicker','#FF0000');
+pref('extensions.snaplinks.linksthick',1);
+pref('extensions.snaplinks.drawpicker','#30AF00');
+pref('extensions.snaplinks.drawthick',3);
