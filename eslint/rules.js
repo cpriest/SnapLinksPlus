39057af7b1574e0d5ddcfaaeb353fc213b403be1
Clint Priest <snaplinks@dev.rxv.me>
2019-09-09 17:57:56 -0500

    Added eslint based off of jquery.

---
+++ eslint/rules.js
@@ -0,0 +1,22 @@
+module.exports = {
+	'rules': {
+		'no-tabs':                     'off',
+		'space-before-function-paren': [2, { 'anonymous': 'always', 'named': 'never' }],
+		'space-in-parens':             'off',
+		'quotes':                      'off',
+		'curly':                       'off',
+		'keyword-spacing':             'off',
+		'no-unused-vars':              ['error', { 'vars': 'local', 'args': 'none' }],
+		'array-bracket-spacing':       ['error', 'never'],
+		'computed-property-spacing':   'off',
+		'lines-around-comment':        'off',
+		'brace-style':                 'off',
+		'max-len':                     ['error', 180, 4, { 'ignoreUrls': true }],
+		'eqeqeq':                      'off',
+		'key-spacing':                 ['error', { 'multiLine': { 'align': 'value' } }],
+		'comma-dangle':                ['error', 'always-multiline'],
+		'no-nested-ternary':           'off',
+		'no-mixed-spaces-and-tabs':    ['error', 'smart-tabs'],
+		'operator-linebreak':          ['error', 'after', { 'overrides': { '?': 'before', ':': 'before' } }],
+	},
+};
