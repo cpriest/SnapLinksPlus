f5d905c9302d7881c2eb15e28627c015d139b89d
Clint Priest <snaplinks@dev.rxv.me>
2021-08-27 20:25:27 -0500

    Adds working implementation of natural tab opening order.  Slightly stickier than Firefox is, perhaps a benefit.
    Closes #332

--- src/background-scripts/background.js
+++ src/background-scripts/background.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let allTabStacks = new Map();
+
 /**
  * Log that we received the message.
  * Then display a notification. The notification contains the URL,
@@ -42,45 +44,76 @@ async function OpenUrlsInTabs(urls) {
 		currentWindow: true,
 	});
 
-	let activeTab = (async () => {
-		let tabs = await browser.tabs.query({
-			active:        true,
-			currentWindow: true,
-		});
-		if(!tabs.length)
-			return;
-		return tabs[0];
-	})();
+	let activeTab = await browser.tabs.query({
+		active:        true,
+		currentWindow: true,
+	})[0];
 
 	let tabsLeft   = urls.length,
-		tabsOpened = 0;
+		tabsOpened = 0,
+		startIndex;
+
+	switch(Prefs.OpenTabs) {
+		case TABS_OPEN_END:
+			startIndex = tabs.length;
+			break;
+		case TABS_OPEN_RIGHT:
+			startIndex = activeTab.index + 1;
+			break;
+		case TABS_OPEN_NATURAL:
+			/* Naturally means like Firefox does with middle-click on a URL as of 2021-08-27
+			 *
+			 *	 This means if you are on Tab A with a Tab B & C open, such as [A] B C and then:
+			 *	 * You middle click 2 links							-> [A] A.1 A.2      B      C
+			 *	 * You switch to tab B and middle click 1 link		->  A  A.1 A.2     [B] B.1 C
+			 *	 * You switch back to tab A and middle-click 1 link	-> [A] A.3 A.1 A.2 [B] B.1 C
+			 *
+			 *	 This means "open at end of {current stack}" where {current stack} means the last opened tab
+			 *	 from the current tab with the stack clearing upon changing tabs.
+			 */
+
+			break;
+	}
+
+	let tabStack = allTabStacks.get(activeTab.id) || [];
+
+	let resolvedTab = await tabStack.reduce(async (memo, tabId) => {
+		try {
+			if(await memo)
+				return memo;
+		} catch(error) { /* ignored */ }
+		try { return browser.tabs.get(tabId); } catch(error) { /* ignored */ }
+
+		return memo;
+	}, undefined);
+
+	if(Prefs.OpenTabs == TABS_OPEN_NATURAL) {
+		startIndex = (resolvedTab?.index || activeTab.index) + 1;
+	}
 
 	for(let url of urls) {
 		let props = {
 			url:    url,
 			active: Prefs.SwitchFocusToNewTab ? (--tabsLeft) === 0 : false,	// Activate the last tab to be opened
+			index:  startIndex + tabsOpened++,
 		};
-		switch(Prefs.OpenTabs) {
-			case TABS_OPEN_END:
-				props.index = tabs.length + tabsOpened++;
-				break;
-			case TABS_OPEN_RIGHT:
-				props.index = activeTab.index + 1 + tabsOpened++;
-				break;
-			case TABS_OPEN_NATURAL:
 
-				break;
-		}
 		if(isFirefox) {
 			props.cookieStoreId = activeTab.cookieStoreId;
 			if(Prefs.SetOwnershipTabID_FF)
 				props.openerTabId = activeTab.id;
 		}
 
-		//noinspection ES6MissingAwait
-		browser.tabs.create(props);
+		let newTab = await browser.tabs.create(props);
+
+		if(Prefs.OpenTabs == TABS_OPEN_NATURAL) {
+			tabStack.unshift(newTab.id);
+		}
+
 		await sleep(Prefs.NewTabDelayMS);
 	}
+	if(Prefs.OpenTabs == TABS_OPEN_NATURAL)
+		allTabStacks.set(activeTab.id, tabStack);
 }
 
 /**
