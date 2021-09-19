f87b3ba4e031b9c4cbb9d89851df49576beadd20
Clint Priest <snaplinks@dev.rxv.me>
2021-09-19 14:22:09 -0500

    Package updates

--- .webext-ff.js
+++ .webext-ff.js
@@ -1,12 +1,12 @@
 module.exports = {
 	sourceDir: './build/ff/',
 	run: {
-		firefoxProfile:         `${__dirname}/local/SnapLinksDev`,
+		firefoxProfile:         `SnapLinksDev`,
 		profileCreateIfMissing: true,
 		keepProfileChanges:     true,
 		reload:                 true,
-		startUrl:               [`file://${__dirname}/test/test_links.html`, 'about:addons', 'about:devtools-toolbox?id=snaplinks%40snaplinks.mozdev.org&type=extension'],
-		watchFile:              [`${__dirname}\\build\\build.json`],
-		firefox:                'firefoxdeveloperedition'
+//		startUrl:               [`file://${__dirname}/test/test_links.html`, 'about:addons', 'about:devtools-toolbox?id=snaplinks%40snaplinks.mozdev.org&type=extension'],
+		watchFile: [`${__dirname}\\build\\build.json`],
+		firefox:   'firefoxdeveloperedition'
 	},
 };
